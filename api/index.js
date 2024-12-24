b06ae61a670ed65f1b282efc9d8baf74f0f3859e
Prathamesh Lakare <prathameshlakare001@gmail.com>
2024-12-24 18:53:48 +0530

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -62,7 +62,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/auth/google", (req, res) => {
-  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.BACKEND_URL}/auth/google/callback&response_type=code&scope=profile email`;
   res.redirect(googleAuthUrl);
 });
 
@@ -82,7 +82,7 @@ app.get(`/auth/google/callback`, async (req, res) => {
         client_secret: process.env.GOOGLE_CLIENT_SECRET,
         code,
         grant_type: "authorization_code",
-        redirect_uri: `${process.env.FRONTEND_URL}/auth/google/callback`,
+        redirect_uri: `${process.env.BACKEND_URL}/auth/google/callback`,
       },
       {
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
