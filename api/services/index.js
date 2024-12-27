50fbfe8a0e8d58910b931076f52a6051e868132e
Prathamesh Lakare <prathameshlakare001@gmail.com>
2024-12-27 18:21:49 +0530

    modified:   api/services/index.js

--- api/services/index.js
+++ api/services/index.js
@@ -1,7 +1,6 @@
 function setSecureCookie(res, token) {
   res.cookie("access_token", token, {
     httpOnly: false,
-    secure: true,
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: "none",
   });
